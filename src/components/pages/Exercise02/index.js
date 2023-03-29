6ccc7208b5fb8c5be09f36d01573b5c0d57a5ee7
Joel Bermudez Acosta <47182906+JoelBerus@users.noreply.github.com>
2023-03-29 17:58:20 -0500

    fixed 2nd challenge
    Se resolvio el segundo reto con exito y se dejo lo mas parecido el diseño proporcionado

--- src/components/pages/Exercise02/index.js
+++ src/components/pages/Exercise02/index.js
@@ -17,27 +17,69 @@ import { useEffect, useState } from "react";
 
 export default function Exercise02 () {
   const [movies, setMovies] = useState([])
-  const [fetchCount, setFetchCount] = useState(0)
-  const [loading, setLoading] = useState(false)
+  const [movieFilters, setMovieFilters] = useState([]);
+  const [sortByText, setSortByText] = useState('Year Descending'); // Year Descending || Year Ascending
+  const [fetchCount, setFetchCount] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  const handleMovieFetch = () => {
+  const handleMovieFilter = () => {
+    setLoading(true)
+    fetch('http://localhost:3001/genres')
+      .then(res => res.json())
+      .then(json => {
+        setMovieFilters(json)
+        setLoading(false)
+      })
+      .catch(() => {
+        setMovieFilters({})
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  const handleMovieFilterSelect = (e) => {
+    const value = e.target.value !== '' ? e.target.value : null
+    handleMovieFetch(value);
+  }
+
+  const handleMovieFetch = (filter = null) => {
     setLoading(true)
     setFetchCount(fetchCount + 1)
     console.log('Getting movies')
-    fetch('http://localhost:3001/movies?_limit=50')
+    if (filter !== null){
+      console.log('filter', filter)
+    }
+    fetch('http://localhost:3001/movies?_limit=50' + (filter !== null ? `&genres_like=${filter}` : '') )
       .then(res => res.json())
       .then(json => {
         setMovies(json)
         setLoading(false)
       })
       .catch(() => {
-        console.log('Run yarn movie-api for fake api')
+        setMovies([])
+      })
+      .finally(() => {
+        setLoading(false)
       })
   }
 
+  const sortMoviesByYear = () => {
+    if (sortByText === 'Year Descending') {
+      const sortedMovies = [...movies].sort((a, b) => a.year - b.year)
+      setMovies(sortedMovies)
+      setSortByText('Year Ascending')
+    }else {
+      const sortedMovies = [...movies].sort((a, b) => b.year - a.year)
+      setMovies(sortedMovies)
+      setSortByText('Year Descending')
+    }
+  }
+
   useEffect(() => {
-    handleMovieFetch()
-  }, [handleMovieFetch])
+    handleMovieFetch();
+    handleMovieFilter();
+  }, [])
 
   return (
     <section className="movie-library">
@@ -45,10 +87,15 @@ export default function Exercise02 () {
         Movie Library
       </h1>
       <div className="movie-library__actions">
-        <select name="genre" placeholder="Search by genre...">
-          <option value="genre1">Genre 1</option>
+        <select name="genre" onChange={handleMovieFilterSelect} className="movie-library__actions__dropdown">
+          <option value="">Select a genre</option>
+          {movieFilters.length > 0 && movieFilters.map((genre, index) => (
+            <option key={index} value={genre}>{genre}</option>
+          ))}
         </select>
-        <button>Order Descending</button>
+        <button onClick={() => sortMoviesByYear()} className="movie-library__actions__button">
+          {sortByText}
+        </button>
       </div>
       {loading ? (
         <div className="movie-library__loading">
@@ -57,16 +104,16 @@ export default function Exercise02 () {
         </div>
       ) : (
         <ul className="movie-library__list">
-          {movies.map(movie => (
+          {movies.length && movies.map(movie => (
             <li key={movie.id} className="movie-library__card">
-              <img src={movie.posterUrl} alt={movie.title} />
-              <ul>
-                <li>ID: {movie.id}</li>
-                <li>Title: {movie.title}</li>
-                <li>Year: {movie.year}</li>
-                <li>Runtime: {movie.runtime}</li>
-                <li>Genres: {movie.genres.join(', ')}</li>
-              </ul>
+              <img className="movie-library__card-image" src={movie.posterUrl} alt={movie.title} loading="lazy" />
+              <div className="movie-library__card-copy-container">
+                <ul className="movie-library__card-copy" >
+                  <li className="movie-library-copy__title">{movie.title}</li>
+                  <li className="movie-library-copy__genres">{movie.genres.join(', ')}</li>
+                  <li className="movie-library-copy__year">{movie.year}</li>
+                </ul>
+              </div>
             </li>
           ))}
         </ul>
